ab083538a1785e115c340df8dbc82587ffe41aec
olamo <ollikarppinen@gmail.com>
2015-02-19 15:37:17 +0200

    Added two objects for the simulation panel and the settings panel

--- src/boids/Gui.scala
+++ src/boids/Gui.scala
@@ -1,7 +1,16 @@
 package boids
 
 import scala.swing._
+import scala.swing.BorderPanel.Position._
 
 object Gui extends SimpleSwingApplication{
-  val top = new MainFrame
+  val top = new MainFrame {
+    title = "Boid simulation"
+    contents = new BorderPanel {
+      layout(SimulationPanel) = Center
+      layout(SettingsPanel) = East
+    }
+
+  }
 }
+
