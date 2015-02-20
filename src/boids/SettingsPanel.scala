c50527c2c18760aa0c8523e17f1d4be35b8abcdf
olamo <ollikarppinen@gmail.com>
2015-02-21 01:48:52 +0200

    Couple new classes that were not pushed earlier.

---
+++ src/boids/SettingsPanel.scala
@@ -0,0 +1,13 @@
+package boids
+
+import scala.swing._
+import java.awt.{ Graphics2D, Color, Dimension }
+
+object SettingsPanel extends FlowPanel {
+  background = Color.lightGray
+  preferredSize = new Dimension(100, 500)
+  contents += new Label {
+    text = "Settings"
+  }
+
+}
---
+++ src/boids/SimulationPanel.scala
@@ -0,0 +1,26 @@
+package boids
+
+import scala.swing.Panel
+import java.awt.{ Graphics2D, Color, Dimension }
+
+object SimulationPanel extends Panel {
+  preferredSize = new Dimension(501, 501)
+  opaque = true
+  background = Color.black
+  override def paintComponent(g: Graphics2D) {
+    super.paintComponent(g)
+    g.setColor(Color.darkGray)
+
+    for (x <- 0 to 50; y <- 0 to 50) { // draws the grid
+      g.drawLine(0, x * 10, 500, x * 10)
+      g.drawLine(x * 10, 0, x * 10, 500)
+    }
+
+    def drawBoid(b: Boid) = {
+    	g.setColor(Color.white)
+      g.drawOval(b.getX - 6, b.getY - 6, 5, 5)   
+    }
+    simulation.flock.foreach(b => drawBoid(b))
+  }
+  new Thread(simulation).start()
+}
