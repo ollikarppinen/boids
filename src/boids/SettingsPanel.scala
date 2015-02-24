269c4bd1f49db5bea8a370568c3145e2e7584947
olamo <ollikarppinen@gmail.com>
2015-02-24 20:40:29 +0200

    Changed the color theme.

--- src/boids/SettingsPanel.scala
+++ src/boids/SettingsPanel.scala
@@ -4,7 +4,7 @@ import scala.swing._
 import java.awt.{ Graphics2D, Color, Dimension }
 
 object SettingsPanel extends FlowPanel {
-  background = new Color(238, 238, 238)
+  background = Color.white
   preferredSize = new Dimension(200, 1000)
   
   val massSlider = new Slider {
@@ -12,6 +12,7 @@ object SettingsPanel extends FlowPanel {
     min = 1
     max = 100
     value = 10
+    background = Color.white
     minimumSize = new Dimension(200, 300)
   }
 
@@ -20,6 +21,7 @@ object SettingsPanel extends FlowPanel {
     min = 1
     max = 200
     value = 10
+    background = Color.white
     minimumSize = new Dimension(200, 300)
   }
   
@@ -28,6 +30,7 @@ object SettingsPanel extends FlowPanel {
     min = 1
     max = 100
     value = 10
+    background = Color.white
     minimumSize = new Dimension(200, 300)
   }
   
@@ -36,6 +39,7 @@ object SettingsPanel extends FlowPanel {
     min = 1
     max = 100
     value = 10
+    background = Color.white
     minimumSize = new Dimension(200, 300)
   }
   
@@ -44,17 +48,20 @@ object SettingsPanel extends FlowPanel {
     min = 0
     max = 1000
     value = 200
+    background = Color.white
     minimumSize = new Dimension(200, 300)
   }
   
   val pauseButton = new Button {
     text = "Pause"
     preferredSize = new Dimension(180, 30)
+    background = Color.white
   }
   
   val restartButton = new Button {
     text = "Restart"
     preferredSize = new Dimension(180, 30)
+    background = Color.white
   }
   
   contents += (new Label("Settings"), 
